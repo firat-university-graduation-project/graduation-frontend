7f7a4901495f68f036358e6e0467bb5e7d1a3793
cihat <cihat02020202@gmail.com>
2022-02-14 18:18:38 +0300

    Add home and video route in react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css"
-import Room from "./views/Room"
+import "./styles/App.css"
+import Home from "./views/Home"
+import Video from "./views/Video"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Room />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/video" exact component={Video} />
+        </Switch>
+      </Router>
     </div>
   )
 }
