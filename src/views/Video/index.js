ef68cff4232d9228bb58e9128054cf9f93b30635
cihat <cihat02020202@gmail.com>
2022-02-15 15:08:26 +0300

    Change port number

--- src/views/Video/index.js
+++ src/views/Video/index.js
@@ -20,7 +20,7 @@ import Modal from "react-bootstrap/Modal"
 import "bootstrap/dist/css/bootstrap.css"
 import "./Video.css"
 
-const server_url = "http://localhost:4001"
+const server_url = "http://localhost:4000"
 
 var connections = {}
 const peerConnectionConfig = {
