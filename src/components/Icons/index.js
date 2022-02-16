f85e35b99602a8d86e01020d6675d853c1c7218e
cihat <cihat02020202@gmail.com>
2022-02-16 21:25:16 +0300

    Add icons component

---
+++ src/components/Icons/index.js
@@ -0,0 +1,21 @@
+import VideocamIcon from "@material-ui/icons/Videocam"
+import VideocamOffIcon from "@material-ui/icons/VideocamOff"
+import MicIcon from "@material-ui/icons/Mic"
+import MicOffIcon from "@material-ui/icons/MicOff"
+import ScreenShareIcon from "@material-ui/icons/ScreenShare"
+import StopScreenShareIcon from "@material-ui/icons/StopScreenShare"
+import CallEndIcon from "@material-ui/icons/CallEnd"
+import ChatIcon from "@material-ui/icons/Chat"
+import RadioButtonCheckedIcon from "@material-ui/icons/RadioButtonChecked"
+
+export {
+  VideocamIcon,
+  VideocamOffIcon,
+  MicIcon,
+  MicOffIcon,
+  ScreenShareIcon,
+  StopScreenShareIcon,
+  CallEndIcon,
+  ChatIcon,
+  RadioButtonCheckedIcon,
+}
