213ecf0c6ca53c4081e61d6ccc03d94564199590
cihat <cihat02020202@gmail.com>
2022-02-23 15:35:07 +0300

    Add ModalMessage component

--- src/components/Modal/Message/index.js
+++ src/components/Modal/Message/index.js
@@ -1,10 +1,53 @@
 import React from "react"
+import Modal from "react-bootstrap/Modal"
+import { Input, Button } from "@material-ui/core"
 
-const MessageModal = () => {
+const MessageModal = (props) => {
+  const {
+    showModal,
+    messages,
+    message,
+    handleMessage,
+    sendMessage,
+    closeChat,
+  } = props
+
   return (
-    <div>
-      <p>MessageModal</p>
-    </div>
+    <Modal show={showModal} onHide={closeChat} style={{ zIndex: "999999" }}>
+      <Modal.Header closeButton>
+        <Modal.Title>Chat Room</Modal.Title>
+      </Modal.Header>
+      <Modal.Body
+        style={{
+          overflow: "auto",
+          overflowY: "auto",
+          height: "400px",
+          textAlign: "left",
+        }}
+      >
+        {messages.length > 0 ? (
+          messages.map((item, index) => (
+            <div key={index} style={{ textAlign: "left" }}>
+              <p style={{ wordBreak: "break-all" }}>
+                <b>{item.sender}</b>: {item.data}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>No message yet</p>
+        )}
+      </Modal.Body>
+      <Modal.Footer className="div-send-msg">
+        <Input
+          placeholder="Message"
+          value={message}
+          onChange={(e) => handleMessage(e)}
+        />
+        <Button variant="contained" color="primary" onClick={sendMessage}>
+          Send
+        </Button>
+      </Modal.Footer>
+    </Modal>
   )
 }
 
