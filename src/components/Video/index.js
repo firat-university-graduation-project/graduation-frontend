0015f29042f8d8aea6f46a42a6d87918f5d201b2
cihat <cihat02020202@gmail.com>
2022-02-23 18:47:16 +0300

    Remove Container wrap in Video Component

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import CopyUrl from "../CopyUrl"
 import { Row, Container } from "reactstrap"
+import "./video.css"
 
 const Video = (props) => {
   const { video, localVideoref, username } = props
@@ -8,34 +9,32 @@ const Video = (props) => {
     <div className="video_container">
       <CopyUrl />
 
-      <Container>
-        <Row
-          id="main"
-          className="flex-container"
-          style={{ margin: 0, padding: 0 }}
-        >
-          {video == true ? (
-            <video
-              id="my-video"
-              ref={localVideoref}
-              autoPlay
-              muted
-              style={{
-                borderStyle: "solid",
-                borderColor: "#bdbdbd",
-                margin: "10px",
-                objectFit: "fill",
-                width: "50%",
-                height: "50%",
-              }}
-            ></video>
-          ) : (
-            <div style={{ backgroundColor: "red" }}>
-              <h2>{username || "Username"}</h2>
-            </div>
-          )}
-        </Row>
-      </Container>
+      <Row
+        id="main"
+        className="flex-container"
+        style={{ margin: 0, padding: 0 }}
+      >
+        {video == true ? (
+          <video
+            id="my-video"
+            ref={localVideoref}
+            autoPlay
+            muted
+            style={{
+              borderStyle: "solid",
+              borderColor: "#bdbdbd",
+              margin: "10px",
+              objectFit: "fill",
+              width: "50%",
+              height: "50%",
+            }}
+          ></video>
+        ) : (
+          <div style={{ backgroundColor: "red" }}>
+            <h2>{username || "Username"}</h2>
+          </div>
+        )}
+      </Row>
     </div>
   )
 }
