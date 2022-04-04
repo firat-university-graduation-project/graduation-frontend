f06754716dc2348ce0b503063d1e57d8272436e8
cihat <cihat02020202@gmail.com>
2022-04-04 12:50:55 +0300

    Update CopyUrl component that add qr code

--- src/components/CopyUrl/index.js
+++ src/components/CopyUrl/index.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { message } from "antd"
 import { FileCopyIcon } from "../Icons"
 import { IconButton } from "@material-ui/core"
+import Modal from "react-bootstrap/Modal"
+import QRCode from "../QRCode"
+import { Button } from "@material-ui/core"
+import { saveAs } from "file-saver"
 
 const copy_url = () => {
   let text = window.location.href
@@ -31,10 +35,38 @@ const copy_url = () => {
 }
 
 const CopyUrl = () => {
+  const [show, setShow] = useState(false)
+  const [qrURL, setQrURL] = useState("")
+
+  console.log("qrUrl: ", qrURL)
+
+  const download_qr = () => {
+    saveAs(qrURL, "qr-code.jpg") // Put your image url here.
+  }
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   return (
-    <IconButton onClick={copy_url} style={{ color: "#424242" }}>
-      <FileCopyIcon />
-    </IconButton>
+    <span>
+      <IconButton onClick={handleShow} style={{ color: "#424242" }}>
+        <FileCopyIcon />
+      </IconButton>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>QR Code</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <QRCode type="modal" setQrURL={setQrURL} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={download_qr}>Download QR Code</Button>
+          <Button onClick={copy_url}>Copy Url</Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </span>
   )
 }
 
